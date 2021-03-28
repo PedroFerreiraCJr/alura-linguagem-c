305aa05724d9b70583f3420581aea3c0f679dda7
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-03-28 18:09:44 -0300

    declaração da variável que armazenará o numero secreto

--- main.c
+++ main.c
@@ -13,6 +13,11 @@ int main(int argc, char *argv[]) {
     printf("* Seja bem-vindo ao nosso Jogo de Adivinhação *\n");
     printf("***********************************************\n");
 
+    // declaração e inicialização de variável do tipo inteiro
+    int numero_secreto = 42;
+
+    printf("O número %d é o numero secreto. Não conta para ninguém!\n", numero_secreto);
+
     // instrução de retorno de valores ao chamadar da função main, que é
     //o sistema operacional. nesse caso o código de sáida é conclusão normal
     return 0;
