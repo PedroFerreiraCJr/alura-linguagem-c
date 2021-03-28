853eb6e9a990c8071593858e6566673f41d367e6
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-03-28 18:06:29 -0300

    implementação do cabeçalho do jogo de adivinhação

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
+// adição de biblioteca de I/O
 #include <stdio.h>
 
 /*
-    compilação: gcc -Wall -o main main.c
+    @Autor: Pedro Junior
+    @Instrucoes: como compilar no terminal: gcc -o main main.c
 */
 
+// função principal, ponto de entrada do programa
 int main(int argc, char *argv[]) {
-    printf("Seja bem-vindo ao nosso Jogo de Adivinhação\n");
+    // imprime o cabeçalho do programa
+    printf("***********************************************\n");
+    printf("* Seja bem-vindo ao nosso Jogo de Adivinhação *\n");
+    printf("***********************************************\n");
+
+    // instrução de retorno de valores ao chamadar da função main, que é
+    //o sistema operacional. nesse caso o código de sáida é conclusão normal
     return 0;
 }
