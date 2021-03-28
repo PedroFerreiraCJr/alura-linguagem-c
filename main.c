87c2ad0a67110cb553a9c4753df5c6f0df564457
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-03-28 18:35:52 -0300

    implementação da leitura de valores do teclado

--- main.c
+++ main.c
@@ -16,7 +16,16 @@ int main(int argc, char *argv[]) {
     // declaração e inicialização de variável do tipo inteiro
     int numero_secreto = 42;
 
-    printf("O número %d é o numero secreto. Não conta para ninguém!\n", numero_secreto);
+    // declaração e inicialização da variável que armazenará o
+    //o valor do chute o jogador
+    int chute = 0;
+    // imprime na tela a pergunta sobre qual valor o jogador quer fornecer
+    printf("Qual é o seu chute?: ");
+    // lê do teclado o valor digitado pelo jogador
+    scanf("%d", &chute);
+
+    // imprime o valor fornecido pelo jogador
+    printf("Seu chute foi %d\n", chute);
 
     // instrução de retorno de valores ao chamadar da função main, que é
     //o sistema operacional. nesse caso o código de sáida é conclusão normal
