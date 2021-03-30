0919282256fe8dc0ab9e61a2ce86bb327a0b3a54
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-03-29 23:57:28 -0300

    implementação de tentativas de jogo

--- main.c
+++ main.c
@@ -19,32 +19,42 @@ int main(int argc, char *argv[]) {
     // declaração e inicialização da variável que armazenará o
     //o valor do chute o jogador
     int chute = 0;
-    // imprime na tela a pergunta sobre qual valor o jogador quer fornecer
-    printf("Qual é o seu chute?: ");
-    // lê do teclado o valor digitado pelo jogador
-    scanf("%d", &chute);
 
-    // imprime o valor fornecido pelo jogador
-    printf("Seu chute foi %d.\n", chute);
+    // loop for para perguntar ao jogador, com três tentativas
+    //três repetições no máximo
+    for (int i=1; i<=3; i++) {
+        printf("Tentativa %d de 3\n", i);
+        // imprime na tela a pergunta sobre qual valor o jogador quer fornecer
+        printf("Qual é o seu chute?: ");
+        // lê do teclado o valor digitado pelo jogador
+        scanf("%d", &chute);
 
-    // implementação de verificação condicional, com ramificação, caso
-    //verdadeiro e caso falso.
-    if (chute == numero_secreto) {
-        // caso os números forem iguais, o jogador acertou
-        printf("Parabéns! Você acertou!\n");
-        printf("Jogue de novo, você é um bom jogador.\n");
-    }
-    else {
-        // faz a verificação utilizando operadores relacionais para
-        //dizer ao jogador quão perto o valor está do numero secreto
-        int maior = chute > numero_secreto;
-        if (maior) {
-            printf("Seu chute foi maior que o número secreto.\n");
+        // imprime o valor fornecido pelo jogador
+        printf("Seu chute foi %d.\n", chute);
+
+        // implementação de verificação condicional, com ramificação, caso
+        //verdadeiro e caso falso.
+        if (chute == numero_secreto) {
+            // caso os números forem iguais, o jogador acertou
+            printf("Parabéns! Você acertou!\n");
+            printf("Jogue de novo, você é um bom jogador.\n");
         }
         else {
-            printf("Seu chute foi menor que o número secreto.\n");
+            // faz a verificação utilizando operadores relacionais para
+            //dizer ao jogador quão perto o valor está do numero secreto
+            int maior = chute > numero_secreto;
+            if (maior) {
+                printf("Seu chute foi maior que o número secreto.\n");
+            }
+            else {
+                printf("Seu chute foi menor que o número secreto.\n");
+            }
         }
+
+        printf("\n");
     }
+
+    printf("Fim de jogo.\n");
 
     // instrução de retorno de valores ao chamadar da função main, que é
     //o sistema operacional. nesse caso o código de sáida é conclusão normal
