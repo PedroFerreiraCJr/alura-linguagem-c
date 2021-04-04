4676b6046c6681d451c27d432ab5a026582d94c5
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-04-04 05:35:03 -0300

    implementação de aguardo condicional de threads em c

--- main.c
+++ main.c
@@ -1,71 +1,53 @@
-// adição de biblioteca de I/O
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 /*
     @Autor: Pedro Junior
-    @Instrucoes: como compilar no terminal: gcc -o main main.c
+    @Instrucoes: como compilar no terminal: gcc -o main main.c -lpthread
 */
 
+pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-// função principal, ponto de entrada do programa
-int main(int argc, char *argv[]) {
-    // imprime o cabeçalho do programa
-    printf("***********************************************\n");
-    printf("* Seja bem-vindo ao nosso Jogo de Adivinhação *\n");
-    printf("***********************************************\n");
+int done = 1;
 
-    // declaração e inicialização de variável do tipo inteiro
-    int numero_secreto = 42;
+void *execute() {
+    printf("Thread adquirindo o lock.\n");
+    pthread_mutex_lock(&lock);
 
-    // declaração e inicialização da variável que armazenará o
-    //o valor do chute o jogador
-    int chute = 0;
-    int tentativas = 1;
-
-    while (1) {
-        printf("Tentativa %d\n", tentativas);
-        // imprime na tela a pergunta sobre qual valor o jogador quer fornecer
-        printf("Qual é o seu chute?: ");
-        // lê do teclado o valor digitado pelo jogador
-        scanf("%d", &chute);
-
-        // imprime o valor fornecido pelo jogador
-        printf("Seu chute foi %d.\n", chute);
-
-        if (chute < 0) {
-            printf("Você não pode chutar números negativos...\n");
-            continue;
-        }
-
-        int acertou = chute == numero_secreto;
-        int maior = chute > numero_secreto;
-
-        // implementação de verificação condicional, com ramificação, caso
-        //verdadeiro e caso falso.
-        if (acertou) {
-            // caso os números forem iguais, o jogador acertou
-            printf("Parabéns! Você acertou!\n");
-            printf("Jogue de novo, você é um bom jogador.\n");
-
-            break;
-        }
-        // verifica se o valor do chute é maior que o numero_secreto
-        //então mostra a mensagem do bloco
-        else if (maior) {
-            printf("Seu chute foi maior que o número secreto.\n");
-        }
-        else {
-            printf("Seu chute foi menor que o número secreto.\n");
-        }
-
-        printf("\n");
-        tentativas++;
+    if (done == 1) {
+        done = 2;
+        printf("A primeira thread está aguardando na variável 'condition'.\n");
+        pthread_cond_wait(&condition, &lock);
+    }
+    else {
+        printf("A segunda thead está sinalizando na variável 'condition'.\n");
+        pthread_cond_signal(&condition);
     }
 
-    printf("Fim de jogo.\n");
-    printf("Você acertou em %d tentativas.\n", tentativas);
+    pthread_mutex_unlock(&lock);
+    printf("Retornando da função invocada.\n");
 
-    // instrução de retorno de valores ao chamadar da função main, que é
-    //o sistema operacional. nesse caso o código de sáida é conclusão normal
+    return NULL;
+}
+
+// função principal, ponto de entrada do programa
+int main(int argc, const char *argv[]) {
+    printf("Criando primeira thread.\n");
+    pthread_t tids[2];
+    pthread_create(&tids[0], NULL, execute, NULL);
+
+    printf("Aguardando 5s antes de criar a segunda thread.\n");
+    sleep(10);
+
+    printf("Segunda thread criada.\n");
+    pthread_create(&tids[1], NULL, execute, NULL);
+
+    pthread_join(tids[0], NULL);
+    pthread_join(tids[1], NULL);
+
+    printf("As threads finalizaram.\n");
+
     return 0;
 }
