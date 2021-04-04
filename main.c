089672c498d7109a27760de28a64e9d2b0a5a858
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-04-04 04:58:50 -0300

    implementação simples de usos de threads em c.

--- main.c
+++ main.c
@@ -1,71 +1,34 @@
-// adição de biblioteca de I/O
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 /*
     @Autor: Pedro Junior
-    @Instrucoes: como compilar no terminal: gcc -o main main.c
+    @Instrucoes: como compilar no terminal: gcc -o main main.c -lpthread
 */
 
+void *print(void *value) {
+    int *i = malloc(sizeof(int));
+    *i = *(int *)value;
+    printf("O valor passado como argumento para a tarefa foi: %d.\n", *i);
+    (*i) *= 10;
+
+    pthread_exit(i);
+}
 
 // função principal, ponto de entrada do programa
-int main(int argc, char *argv[]) {
-    // imprime o cabeçalho do programa
-    printf("***********************************************\n");
-    printf("* Seja bem-vindo ao nosso Jogo de Adivinhação *\n");
-    printf("***********************************************\n");
+int main(int argc, const char *argv[]) {
+    int valor = 100;
+    pthread_t id;
 
-    // declaração e inicialização de variável do tipo inteiro
-    int numero_secreto = 42;
+    pthread_create(&id, NULL, print, &valor);
 
-    // declaração e inicialização da variável que armazenará o
-    //o valor do chute o jogador
-    int chute = 0;
-    int tentativas = 1;
+    int *p = NULL;
 
-    while (1) {
-        printf("Tentativa %d\n", tentativas);
-        // imprime na tela a pergunta sobre qual valor o jogador quer fornecer
-        printf("Qual é o seu chute?: ");
-        // lê do teclado o valor digitado pelo jogador
-        scanf("%d", &chute);
+    pthread_join(id, (void**)&p);
+    printf("Valor recebido do processo filho: %d.\n", *p);
 
-        // imprime o valor fornecido pelo jogador
-        printf("Seu chute foi %d.\n", chute);
+    free(p);
 
-        if (chute < 0) {
-            printf("Você não pode chutar números negativos...\n");
-            continue;
-        }
-
-        int acertou = chute == numero_secreto;
-        int maior = chute > numero_secreto;
-
-        // implementação de verificação condicional, com ramificação, caso
-        //verdadeiro e caso falso.
-        if (acertou) {
-            // caso os números forem iguais, o jogador acertou
-            printf("Parabéns! Você acertou!\n");
-            printf("Jogue de novo, você é um bom jogador.\n");
-
-            break;
-        }
-        // verifica se o valor do chute é maior que o numero_secreto
-        //então mostra a mensagem do bloco
-        else if (maior) {
-            printf("Seu chute foi maior que o número secreto.\n");
-        }
-        else {
-            printf("Seu chute foi menor que o número secreto.\n");
-        }
-
-        printf("\n");
-        tentativas++;
-    }
-
-    printf("Fim de jogo.\n");
-    printf("Você acertou em %d tentativas.\n", tentativas);
-
-    // instrução de retorno de valores ao chamadar da função main, que é
-    //o sistema operacional. nesse caso o código de sáida é conclusão normal
     return 0;
 }
