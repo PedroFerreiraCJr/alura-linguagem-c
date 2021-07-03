74a3657b3129ddd418f207a1252689fa8d693bf1
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-07-03 12:47:55 -0300

    implementação parcial de um jogo da forca

---
+++ jogo-da-forca/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char palavra_secreta[20];
+    sprintf(palavra_secreta, "Melancia");
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
+
+    do {
+        for(int i=0; i<strlen(palavra_secreta); i++) {
+
+            int achou = 0;
+            for(int j=0; j<tentativas; j++) {
+                if (chutes[j] == palavra_secreta[i]) {
+                    achou = 1;
+                    break;
+                }
+            }
+
+            if (achou) {
+                printf("%c ", palavra_secreta[i]);
+            }
+            else {
+                printf("_ ");
+            }
+        }
+        printf("\n");
+
+        char chute = 0;
+        scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
+    } while(!acertou && !enforcou);
+
+    return 0;
+}
