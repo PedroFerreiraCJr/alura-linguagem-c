4e6ce50dec5e221f938798aa0f4fc40fb7f940e9
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-07-04 22:22:26 -0300

    implementação final do jogo da forca

---
+++ jogo-da-forca/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#define TAMANHO_PALAVRA 20
+
+// protótipo das funções para que o compilador ignore a ordem de implementação da funções
+int letraexiste(char letra);
+int chuteserrados();
+int enforcou();
+int ganhou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
+
+#endif // MAIN_H_INCLUDED
